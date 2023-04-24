5a261f9497107cf96793e6ce0a6821e9ef6780e5
rdiego56@yahoo.com <rdiego56@yahoo.com>
2023-04-23 23:31:55 -0700

    added events in java script

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-
+var eventsData;
 
 function setHourColors(){
     var now = dayjs();
     for (var i = 9; i < 18; i++) {
-        $("#hour-" + i + "textare").removeClass("future");
+        // $("#hour-" + i + "textare").removeClass("future");
         if (i < now.hour()) {
             $("#hour-" + i + "textarea").addClass("past");
         } else if (i == now.hour()) {
@@ -13,12 +13,28 @@ function setHourColors(){
 }
 
 function loadStoredData() {
+    var eventsData = JSON.parse(localStorage.getItem("calenderEvents"));
+    if (!eventsData) {
+        eventsData = {
+            hour9: "",
+            hour10: "",
+            // etc continue on
+        };
+    }
     // Load exisitng data from local storage
 }
 
 function handleSaveClick(event) {
-    var saveButton = $(event.target);
-    // store this hours data in local storage
+    // grab data from HTML
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.children("textarea").val();
+    var hour = hourBlock.after('id').split("-")[1];
+
+    // modify data object
+    eventsData["hour" + hour] = value;
+
+    // store in local storage
+    localStorage.setItem("calenderEvents" , JSON.stringify(eventsData))
 
 }
 
